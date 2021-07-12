10852871f2ee0f59b3ab30a247548a6411daeffd
priyankamishra3096 <priyankamishra3096@gmail.com>
2021-07-12 17:42:03 +0530

    add req.txt

--- test_cases.py
+++ test_cases.py
@@ -18,8 +18,18 @@ def test_handler_no_env_variable(client):
 
 
 def test_handler_with_env_variable(client):
-    os.environ["NAME"] = "Foo"
+    os.environ["NAME"] = "Priyanka"
     r = client.get("/")
 
-    assert r.data.decode() == "Hello Foo!"
+    assert r.data.decode() == "Hello Priyanka!"
     assert r.status_code == 200
+
+# def test_handler_with_env_variable():
+#     os.environ["arg1"] = 10
+#     os.environ["arg2"] = 20
+#     response = requests.post(url, json=data)
+#     print(response)
+#     res_data = json.loads(response.text)
+#     assert int(res_data["Sum"]) == 10
+
+
